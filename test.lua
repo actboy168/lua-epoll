8c12c12312e9183147215c7205852a690629cac6
actboy168 <actboy168@gmail.com>
2021-07-17 16:33:01 +0800

    update test

--- test.lua
+++ test.lua
@@ -1,68 +1,13 @@
-local socket = require "bee.socket"
-local epoll = require "epoll"
-local epfd = epoll.create(512)
+package.path = table.concat({
+    "?.lua",
+    "3rd/ltest/?.lua",
+}, ";")
 
-local EPOLLIN <const> = epoll.EPOLLIN
-local EPOLLOUT <const> = epoll.EPOLLOUT
-local EPOLLERR <const> = epoll.EPOLLERR
-local EPOLLHUP <const> = epoll.EPOLLHUP
+local lt = require "ltest"
 
-local function initfd(fd)
-    local sock = { fd = fd }
-    epfd:event_init(fd:handle(), EPOLLIN | EPOLLOUT, sock)
-    return sock
-end
+require "test.luaref"
+require "test.basic"
+require "test.event"
+require "test.pingpong"
 
-local function closefd(fd)
-    epfd:event_close(fd:handle())
-    fd:close()
-end
-
-local s = assert(socket.bind("tcp", "127.0.0.1", 10086))
-local server = initfd(s)
-function server:on_read()
-    local newfd = assert(self.fd:accept())
-    local session = initfd(newfd)
-    function session:on_read()
-        assert("PING" == self.fd:recv(4))
-    end
-    function session:on_write()
-        self.fd:send "PONG"
-    end
-    function session:on_error()
-        closefd(self.fd)
-    end
-end
-function server:on_error()
-    closefd(self.fd)
-end
-
-local c = assert(socket.connect("tcp", "127.0.0.1", 10086))
-local client = initfd(c)
-function client:on_read()
-    local pong = self.fd:recv(4)
-    print(pong)
-    assert("PONG" == pong)
-end
-function client:on_write()
-    print "PING"
-    self.fd:send "PING"
-end
-function client:on_error()
-    closefd(self.fd)
-end
-
-while true do
-    for fd, event in epfd:wait() do
-        if event & (EPOLLERR | EPOLLHUP) ~= 0 then
-            fd:on_error()
-        else
-            if event & EPOLLIN ~= 0 then
-                fd:on_read()
-            end
-            if event & EPOLLOUT ~= 0 then
-                fd:on_write()
-            end
-        end
-    end
-end
+os.exit(lt.run(), true)
---
+++ test/pingpong.lua
@@ -0,0 +1,219 @@
+local lt = require "ltest"
+
+local m = lt.test "framework"
+local socket = require "bee.socket"
+local epoll = require "epoll"
+local epfd = epoll.create(512)
+
+local EPOLLIN <const> = epoll.EPOLLIN
+local EPOLLOUT <const> = epoll.EPOLLOUT
+local EPOLLERR <const> = epoll.EPOLLERR
+local EPOLLHUP <const> = epoll.EPOLLHUP
+
+local function create_stream(fd)
+    local sock = {fd = fd}
+    local kMaxReadBufSize <const> = 4 * 1024
+    local rbuf = ""
+    local wbuf = ""
+    local closed = false
+    local halfclose_r = false
+    local halfclose_w = false
+    local event_r = false
+    local event_w = false
+    local event_mask = 0
+    local function closefd()
+        epfd:event_close(fd:handle())
+        fd:close()
+    end
+    local function event_update()
+        local mask = 0
+        if event_r then
+            mask = mask & EPOLLIN
+        end
+        if event_w then
+            mask = mask & EPOLLOUT
+        end
+        if mask ~= event_mask then
+            if mask == 0 then
+                epfd:event_del(fd:handle())
+            elseif event_mask == 0 then
+                epfd:event_add(fd:handle(), mask)
+            else
+                epfd:event_mod(fd:handle(), mask)
+            end
+            event_mask = mask
+        end
+    end
+    local function force_close()
+        closefd()
+        assert(halfclose_r)
+        assert(halfclose_w)
+    end
+    local function on_read()
+        local data = fd:recv()
+        if data == nil then
+            halfclose_r = true
+            event_r = false
+            if halfclose_w then
+                force_close()
+            elseif #wbuf == 0 then
+                halfclose_w = true
+                event_w = false
+                force_close()
+            else
+                event_update()
+            end
+        elseif data == false then
+        else
+            rbuf = rbuf .. data
+            if #rbuf > kMaxReadBufSize then
+                event_r = false
+                event_update()
+            end
+            sock:on_recv()
+        end
+    end
+    local function on_write()
+        local n = fd:send(wbuf)
+        if n == nil then
+            halfclose_w = true
+            event_w = false
+            if halfclose_r then
+                force_close()
+            else
+                event_update()
+            end
+        elseif n == false then
+        else
+            wbuf = wbuf:sub(n + 1)
+            if #wbuf == 0 then
+                event_w = false
+                event_update()
+            end
+        end
+    end
+    local function on_event(e)
+        if e & (EPOLLERR | EPOLLHUP) ~= 0 then
+            e = e & (EPOLLIN | EPOLLOUT)
+        end
+        if e & EPOLLIN ~= 0 then
+            assert(not halfclose_r)
+            on_read()
+        end
+        if e & EPOLLOUT ~= 0 then
+            if not halfclose_w then
+                on_write()
+            end
+        end
+    end
+    epfd:event_init(fd:handle(), EPOLLIN | EPOLLOUT, on_event)
+    function sock:send(data)
+        if closed then
+            return
+        end
+        if #data > 0 then
+            wbuf = wbuf .. data
+            event_w = true
+            event_update()
+        end
+    end
+    function sock:recv(n)
+        if n == 0 then
+            return ""
+        end
+        local res
+        local full = #rbuf >= kMaxReadBufSize
+        if n == nil then
+            res = rbuf
+            rbuf = ""
+        elseif n <= #rbuf then
+            res = rbuf:sub(1, n)
+            rbuf = rbuf:sub(n+1)
+        elseif n >= kMaxReadBufSize then
+            res = rbuf
+            rbuf = ""
+        else
+            return
+        end
+        if full then
+            event_r = true
+            event_update()
+        end
+        return res
+    end
+    function sock:close()
+        if closed or halfclose_r then
+            return
+        end
+        halfclose_r = true
+        event_r = false
+        if halfclose_w then
+            force_close()
+        else
+            event_update()
+        end
+    end
+    return sock
+end
+
+local function create_listen(fd)
+    local sock = {fd = fd}
+    local function closefd()
+        epfd:event_close(fd:handle())
+        fd:close()
+    end
+    local function on_event(e)
+        if e & (EPOLLERR | EPOLLHUP) ~= 0 then
+            closefd()
+            return
+        end
+        if e & EPOLLIN ~= 0 then
+            sock:on_accept()
+        end
+    end
+    epfd:event_init(fd:handle(), EPOLLIN, on_event)
+    return sock
+end
+
+function m.test()
+    local quit = false
+    local s = assert(socket.bind("tcp", "127.0.0.1", 10086))
+    local server = create_listen(s)
+    function server:on_accept()
+        local newfd = assert(self.fd:accept())
+        local session = create_stream(newfd)
+        function session:on_recv()
+            local res = self:recv(6)
+            if res == nil then
+                return
+            end
+            local strid = res:match "^PING%-(%d)$"
+            lt.assertIsString(strid)
+            self:send("PONG-"..strid)
+        end
+    end
+
+    local c = assert(socket.connect("tcp", "127.0.0.1", 10086))
+    local client = create_stream(c)
+    client:send "PING-1"
+    function client:on_recv()
+        local res = self:recv(6)
+        if res == nil then
+            return
+        end
+        local strid = res:match "^PONG%-(%d)$"
+        lt.assertIsString(strid)
+        local id = tonumber(strid)
+        if id < 9 then
+            self:send("PING-"..(id+1))
+        else
+            quit = true
+        end
+    end
+
+    while not quit do
+        for f, event in epfd:wait() do
+            f(event)
+        end
+    end
+end
