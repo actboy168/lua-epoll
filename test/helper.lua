569da4b6b17450e957c82982758b0cc7ac08425b
actboy168 <actboy168@gmail.com>
2024-04-16 10:01:51 +0800

    fixes bug

--- test/helper.lua
+++ test/helper.lua
@@ -3,14 +3,14 @@ local socket = require "bee.socket"
 local m = {}
 
 function m.SimpleServer(protocol, ...)
-    local fd = assert(socket(protocol))
+    local fd = assert(socket.create(protocol))
     assert(fd:bind(...))
     assert(fd:listen())
     return fd
 end
 
 function m.SimpleClient(protocol, ...)
-    local fd = assert(socket(protocol))
+    local fd = assert(socket.create(protocol))
     local ok, err = fd:connect(...)
     assert(ok ~= nil, err)
     return fd
